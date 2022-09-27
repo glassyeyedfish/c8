0567694ce738993f576aac14a302e9f4de598726
glassyeyedfish <glassyeyedfish@gmail.com>
2022-09-26 22:23:21 -0400

    Added some CLI args

--- src/c8.c
+++ src/c8.c
@@ -1,9 +1,10 @@
-#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 #include <SDL2/SDL.h>
+
+#define VERSION_STIRNG "0.1.0"
 
 #define FONT_DATA_SIZE (0x10 * 0x5)
 
@@ -36,15 +37,62 @@ unsigned char font_data[FONT_DATA_SIZE] = {
 };
 
 void
-usage(void) {
-        puts("Usage: c8 [FILE]");
+run_rom(void) {
+        int i;
+        for (i = 0x200; i < 0x208; i++) {
+                printf("%x\n", ram[i]);
+                SDL_Delay(16);
+        }
+        putchar('\n');
+}
+
+void
+load_rom(char* filename) {
+        char c;
+        int i;
+        FILE* fp;
+
+        fp = fopen(filename, "r");
+        if(fp == NULL) {
+                printf("Error: Could not load '%s'\n", filename);
+                exit(EXIT_FAILURE);
+        }
+        c = fgetc(fp);
+        i = 0;
+        while (c != EOF) {
+                if (0x200 + i >= 0x1000) {
+                        puts("Error: ROM too large");
+                        exit(EXIT_FAILURE);
+                }
+                ram[0x200 + i] = c;
+                c = fgetc(fp);
+                i++;
+        }
+}
+
+void
+print_usage(void) {
+        puts("Usage: c8 [OPTIONS...] [FILE]");
+}
+
+
+void
+print_version(void) {
+        printf("c8: v%s\n", VERSION_STIRNG);
+}
+
+void 
+print_help(void) {
+        print_usage();
+        putchar('\n');
+        puts("    -h    print this list and exit");
+        puts("    -V    print the version number and exit");
 }
 
 int
 main(int argc, char* argv[]) {
-        char c;
         int i;
-        FILE* fp;
+        char loaded_rom;
 
         /* Set up SDL. */
         SDL_Init(SDL_INIT_VIDEO);
@@ -54,31 +102,27 @@ main(int argc, char* argv[]) {
                 ram[i] = font_data[i];
         }
 
-        /* Load rom. */
-        if (optind >= argc) {
-                usage();
+        /* Process CLI arguments. */
+        loaded_rom = 0;
+        for (i = 1; i < argc && !loaded_rom; i++) {
+                if (strcmp(argv[i], "-h") == 0) {
+                        print_help();
+                        exit(EXIT_SUCCESS);
+                } else if (strcmp(argv[i], "-V") == 0) {
+                        print_version();
+                        exit(EXIT_SUCCESS);
+                } else {
+                        load_rom(argv[i]);
+                        loaded_rom = 1;
+                }
+        }
+
+        if (!loaded_rom) {
+                print_usage();
                 exit(EXIT_FAILURE);
         }
 
-        fp = fopen(argv[optind], "r");
-        c = fgetc(fp);
-        i = 0;
-        while (c != EOF) {
-                if (0x200 + i >= 0x1000) {
-                        puts("Error: ROM too large.");
-                        exit(EXIT_FAILURE);
-                }
-                ram[0x200 + i] = c;
-                c = fgetc(fp);
-                i++;
-        }
-
-        /* Run program. */
-        for (i = 0x200; i < 0x1000; i++) {
-                printf("%x ", ram[i]);
-        }
+        run_rom();
         
-        putchar('\n');
-
         exit(EXIT_SUCCESS);
 }
