5e4eee4884510f659203d82f5d32a3c0c72a467c
glassyeyedfish <glassyeyedfish@gmail.com>
2022-10-04 18:07:14 -0400

    Begin code that evaluates instructions

--- src/c8.c
+++ src/c8.c
@@ -8,18 +8,18 @@
 
 #define VERSION_STIRNG "0.1.0"
 
-#define FONT_DATA_SIZE (0x10 * 0x5)
+#define FONT_DATA_SIZE (16 * 5)
 
 int flag_stepping = 0;
 
 unsigned short pc = 0x200;
 unsigned char ram[0x1000] = { 0 };
-unsigned char v_reg[0x10] = { 0 };
-unsigned short i_reg = 0x1234;
+unsigned char v_reg[16] = { 0 };
+unsigned short i_reg = 0;
 unsigned char call_stack[0x100] = { 0 };
 unsigned char delay_timer = 0;
 unsigned char sound_timer = 0;
-unsigned char display[0x40 * 0x20] = { 0 };
+unsigned long display[32] = { 0 };
 
 unsigned char font_data[FONT_DATA_SIZE] = {
         0xF0, 0x90, 0x90, 0x90, 0xF0,
@@ -40,8 +40,19 @@ unsigned char font_data[FONT_DATA_SIZE] = {
         0xF0, 0x80, 0xF0, 0x80, 0x80
 };
 
-void
-init_rom(sdl_context_t* ctx) {
+/* ROM functions */
+void init_rom(sdl_context_t* ctx);
+void run_rom(void);
+void load_rom(char* filename);
+
+/* Debug printing functions */
+void print_instruction(unsigned int pc);
+
+/* CLI arg print functions */
+void print_usage(void);
+void print_version(void);
+
+void init_rom(sdl_context_t* ctx) {
         int i;
 
         SDL_Init(SDL_INIT_VIDEO);
@@ -65,8 +76,7 @@ init_rom(sdl_context_t* ctx) {
         }
 }
 
-void
-run_rom(void) {
+void run_rom(void) {
         sdl_context_t ctx = { 0 };
 
         /* Initialize everything. */
@@ -80,8 +90,62 @@ run_rom(void) {
 
                 /* Implements the step-through functionality. */
                 if ((flag_stepping && is_key_pressed(&ctx, SDLK_SPACE)) || !flag_stepping) {
-                        printf("%x\n", ram[pc]);
+
+                        /* Seperate instruction into important pieces. */
+                        unsigned int inst = (ram[pc] << 8) | ram[pc + 1];
+                        unsigned int addr = inst & 0x0FFF;
+                        unsigned int byte = inst & 0x00FF;
+                        unsigned int w = (inst & 0xF000) >> 12;
+                        unsigned int x = (inst & 0x0F00) >> 8;
+                        unsigned int y = (inst & 0x00F0) >> 4;
+                        unsigned int z = inst & 0x000F;
+
+                        /* Switch over 'w', which represents the highest 
+                         * nibble. */
+                        switch(w) {
+                        case 0x0:
+                                break;
+                        case 0x1:
+                                break;
+                        case 0x2:
+                                break;
+                        case 0x3:
+                                break;
+                        case 0x4:
+                                break;
+                        case 0x5:
+                                break;
+                        case 0x6:
+                                break;
+                        case 0x7:
+                                break;
+                        case 0x8:
+                                break;
+                        case 0x9:
+                                break;
+                        case 0xA:
+                                break;
+                        case 0xB:
+                                break;
+                        case 0xC:
+                                break;
+                        case 0xD:
+                                break;
+                        case 0xE:
+                                break;
+                        case 0xF:
+                                break;
+                        default:
+                                break;
+                        }
+
+                        printf("%x\n", addr);
                         pc += 2;
+                }
+
+                if (pc >= 0x1000) {
+                        puts("Error: program counter exceeds allocated memory.");
+                        exit(EXIT_FAILURE);
                 }
 
                 SDL_RenderClear(ctx.renderer);
@@ -98,8 +162,10 @@ run_rom(void) {
         SDL_Quit();
 }
 
-void
-load_rom(char* filename) {
+/* Takes a file path and trie to load the bytes into the virtual ram. Prints 
+ * an error and exits if the file doesn't exsist of if the file it too big to 
+ * be loaded. */
+void load_rom(char* filename) {
         char c;
         int i;
         FILE* fp;
@@ -122,8 +188,17 @@ load_rom(char* filename) {
         }
 }
 
-void
-print_usage(void) {
+void print_instruction(unsigned int pc) {
+        unsigned int i;
+        for (i = pc; i <= pc + 1; i++) {
+                if (ram[i] < 0x10) {
+                        putchar('0');
+                }
+                printf("%x", ram[i]);
+        }
+}
+
+void print_usage(void) {
         puts("Usage: c8 [OPTIONS...] [FILE]");
         putchar('\n');
         puts("    -s    step through one instruction at a time");
@@ -132,13 +207,11 @@ print_usage(void) {
 }
 
 
-void
-print_version(void) {
+void print_version(void) {
         printf("c8: v%s\n", VERSION_STIRNG);
 }
 
-int
-main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
         int i;
 
         /* Process CLI arguments. */
